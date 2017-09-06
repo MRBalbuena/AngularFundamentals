5c73960799bedc110bc86a5ef47055ea5b58a107
mbalbuena <mbalbuena@burning-glass.com>
2017-09-06 09:56:36 +0100

    Feature Module - Added user.module.

---
+++ app/user/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({    
+    templateUrl: 'app/user/profile.component.html'
+})
+
+export class ProfileComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
